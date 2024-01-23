82abcdd609768e1c56af073e539aef877f27575f
GiorgioMos <giorgiomosca2@gmail.com>
2024-01-23 17:02:14 +0100

    First commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Dati da config/data.js: {{ $home }}</h1>
+    <h1>Movies</h1>
+    <ul>
+        @foreach ($Movies as $Movie)
+            <li>
+                <span>{{ $Movie->title }}</span>
+                <span>{{ $Movie->original_title }}</span>
+                <span>{{ $Movie->nationality }}</span>
+                <span>{{ $Movie->date }}</span>
+                <span>{{ $Movie->vote }}</span>
+            </li>
+        @endforeach
+    </ul>
     <p>This is my paragraph content.</p>
 @endsection
